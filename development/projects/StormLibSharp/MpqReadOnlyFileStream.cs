0ac9a8d95c36c27f4bc1832a922aec3ef35bdff1
myndfyre <myndfyre@valhallalegends.com>
2011-11-03 03:30:36 +0000

    Initial checkin with project layout.  Includes StormLib downloaded from http://www.zezula.net/en/mpq/download.html#StormLib - note that the page says as of Nov 2, 2011 "The library is free, no license is needed to use it in your projects."  (cited from http://www.zezula.net/en/mpq/stormlib.html )

---
+++ development/projects/StormLibSharp/MpqReadOnlyFileStream.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StormLibSharp
+{
+    internal class MpqReadOnlyFileStream : MpqFileStream
+    {
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, System.IO.SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
